fd5bc932eecbca4d605dc90cff7057c1228ab61a
Ben Hirsh <ben@playgen.com>
2016-06-15 17:30:20 +0100

    Client proxy base class complete

---
+++ src/PlayGen.SGA.ClientAPI/ClientProxy.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace PlayGen.SGA.ClientAPI
+{
+    public class ClientProxy
+    {
+        private string _baseAddress = "https://sga.playgen.com";
+
+
+        private WebClient CreateWebClient()
+        {        
+            return new WebClient { BaseAddress = _baseAddress };
+        }
+
+        public TResponse Get<TResponse>(string urlSuffix)
+        {
+            var client = CreateWebClient();
+
+            using (Stream data = client.OpenRead(urlSuffix))
+            {
+                StreamReader reader = new StreamReader(data);
+                var responseString =  reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<TResponse>(responseString);
+            }
+        }
+
+
+        public TResponse Post<TRequest, TResponse>(string urlSuffix, TRequest payload)
+        {
+
+            var payloadString = JsonConvert.SerializeObject(payload);
+
+            using (var client = CreateWebClient())
+            {
+                client.Headers[HttpRequestHeader.ContentType] = "application/json";
+                var responseString = client.UploadString(urlSuffix, payloadString);
+                return JsonConvert.DeserializeObject<TResponse>(responseString);
+            }
+        }
+
+        public TResponse Put<TRequest, TResponse>(string urlSuffix, TRequest payload)
+        {
+
+            var payloadString = JsonConvert.SerializeObject(payload);
+
+            using (var client = CreateWebClient())
+            {
+                client.Headers[HttpRequestHeader.ContentType] = "application/json";
+                var responseString = client.UploadString(urlSuffix, "PUT", payloadString);
+                return JsonConvert.DeserializeObject<TResponse>(responseString);
+            }
+        }
+
+        public TResponse Delete<TResponse>(string urlSuffix)
+        {
+            using (var client = CreateWebClient())
+            {
+                client.Headers[HttpRequestHeader.ContentType] = "application/json";
+                var responseString = client.UploadString(urlSuffix, "DELETE", "");
+                return JsonConvert.DeserializeObject<TResponse>(responseString);
+            }
+        }
+
+    }
+}
---
+++ src/PlayGen.SGA.ClientAPI/GameClientProxy.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PlayGen.SGA.ClientAPI
+{
+    public class GameClientProxy
+    {
+    }
+}
