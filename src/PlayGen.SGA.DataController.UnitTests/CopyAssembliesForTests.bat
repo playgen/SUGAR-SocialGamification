993ce65fd4308840600f457c397261a0f6d12761
Jared Glass <jared@playgen.com>
2016-06-16 10:12:48 +0100

    bat fixed

--- src/PlayGen.SGA.DataController.UnitTests/CopyAssembliesForTests.bat
+++ src/PlayGen.SGA.DataController.UnitTests/CopyAssembliesForTests.bat
@@ -1,11 +1,15 @@
-﻿%1 :: For some reason this needs to be here or the first attempt to store this in a variable won't work.
+﻿ECHO STARTED: CopyAssembliesForTests
+
+%1 :: For some reason this needs to be here or the first attempt to store this in a variable won't work.
 SET ProjectDir=%1
-SET BinDir=%ProjectDir:~0,-1%\Bin"
+SET BinDir="%ProjectDir%\Bin"
 
-CD %BinDir%
+::CD %BinDir%
 
-FOR /R %%F IN (*) DO (
-  COPY /Y "%%F" .
+%BinDir%
+
+FOR /R %BinDir% %%F IN (*) DO (
+  COPY /Y "%%F" %BinDir%
 )
 
-ECHO DONE
+ECHO COMPLETED: CopyAssembliesForTests
