01595ce8412504bb98565a77f51b3d7067bcf42c
Ben Hirsh <ben@playgen.com>
2016-06-17 13:40:12 +0100

    Added helper functions for URI construction

---
+++ src/PlayGen.SGA.ClientAPI/GroupClientProxy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using PlayGen.SGA.Contracts;
+using PlayGen.SGA.Contracts.Controllers;
+
+namespace PlayGen.SGA.ClientAPI
+{
+    public class GroupClientProxy : ClientProxy, IGroupController
+    {
+        public int Create(Actor actor)
+        {
+            return base.Post<Actor, int>("api/game", actor);
+        }
+
+        public IEnumerable<Actor> Get(string[] name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int[] id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
