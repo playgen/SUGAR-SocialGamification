ef26ef2ea30f5fe9ee27ac0a82a8d826d2e9b4b6
Jared Glass <jared@playgen.com>
2016-06-20 12:38:21 +0100

    initialization

--- src/PlayGen.SGA.WebAPI.UnitTests/Controllers/AccountControllerTests.cs
+++ src/PlayGen.SGA.WebAPI.UnitTests/Controllers/AccountControllerTests.cs
@@ -1,9 +1,29 @@
-﻿using Xunit;
+﻿using PlayGen.SGA.DataController;
+using PlayGen.SGA.DataController.UnitTests;
+using PlayGen.SGA.ServerAuthentication;
+using PlayGen.SGA.WebAPI.Controllers;
+using Xunit;
 
 namespace PlayGen.SGA.WebAPI.UnitTests.Controllers
 {
     public class AccountControllerTests
     {
+        #region Configuration
+        private readonly AccountController _accountController;
+        private readonly UserDbController _userDbController;
+
+        public AccountControllerTests()
+        {
+            _accountController = new AccountController(
+                new AccountDbController(TestDbController.NameOrConnectionString),
+                new UserDbController(TestDbController.NameOrConnectionString),
+                new PasswordEncryption(),
+                new JsonWebTokenUtility("5Y2gQ33IrRffE66030Dy1om5nk4HI58V"));
+
+            _userDbController = new UserDbController(TestDbController.NameOrConnectionString);
+        }
+        #endregion
+
         [Fact]
         public void Test()
         {
