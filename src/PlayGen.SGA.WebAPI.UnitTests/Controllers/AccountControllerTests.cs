1294c0d711531d98e16e0e4374a77ed240c9b2d4
Jared Glass <jared@playgen.com>
2016-06-20 12:26:25 +0100

    account controller mock test

---
+++ src/PlayGen.SGA.WebAPI.UnitTests/Controllers/AccountControllerTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace PlayGen.SGA.WebAPI.UnitTests.Controllers
+{
+    public class AccountControllerTests
+    {
+        [Fact]
+        public void Test()
+        {
+          Assert.True(true);  
+        }
+    }
+}
