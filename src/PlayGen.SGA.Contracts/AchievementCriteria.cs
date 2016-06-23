e52a7a4833f7173f3a422b0bc4581c96d81fde3c
Ellis Spice <ellis@playgen.com>
2016-06-23 17:07:28 +0100

    Enums now returned in JSON as strings

--- src/PlayGen.SGA.Contracts/AchievementCriteria.cs
+++ src/PlayGen.SGA.Contracts/AchievementCriteria.cs
@@ -1,16 +1,19 @@
-﻿namespace PlayGen.SGA.Contracts
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace PlayGen.SGA.Contracts
 {
-    /// <summary>
-    /// Encapsulates requirements for completing an achievement.
-    /// </summary>
-    public class AchievementCriteria
-    {
-        public string Key { get; set; }
+	/// <summary>
+	/// Encapsulates requirements for completing an achievement.
+	/// </summary>
+	public class AchievementCriteria
+	{
+		public string Key { get; set; }
+		[JsonConverter(typeof(StringEnumConverter))]
+		public DataType DataType { get; set; }
+		[JsonConverter(typeof(StringEnumConverter))]
+		public ComparisonType ComparisonType { get; set; }
 
-        public DataType DataType { get; set; }
-
-        public ComparisonType ComparisonType { get; set; }
-
-        public string Value { get; set; }
-    }
+		public string Value { get; set; }
+	}
 }
--- src/PlayGen.SGA.Contracts/SaveDataResponse.cs
+++ src/PlayGen.SGA.Contracts/SaveDataResponse.cs
@@ -1,18 +1,21 @@
-﻿namespace PlayGen.SGA.Contracts
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace PlayGen.SGA.Contracts
 {
-    /// <summary>
-    /// Encapsulates savedata details from the server.
-    /// </summary>
-    public class SaveDataResponse
-    {
-        public int ActorId { get; set; }
+	/// <summary>
+	/// Encapsulates savedata details from the server.
+	/// </summary>
+	public class SaveDataResponse
+	{
+		public int ActorId { get; set; }
 
-        public int GameId { get; set; }
+		public int GameId { get; set; }
 
-        public string Key { get; set; }
+		public string Key { get; set; }
 
-        public string Value { get; set; }
-
-        public DataType DataType { get; set; }
-    }
+		public string Value { get; set; }
+		[JsonConverter(typeof(StringEnumConverter))]
+		public DataType DataType { get; set; }
+	}
 }
