68008a6a9ebb1b66f47dd27c9e0ac4663643f9b8
Jack Coldridge <jack@playgen.com>
2016-07-27 11:21:50 +0100

    remove redundant unit test scripts

--- src/PlayGen.SUGAR.GameData.UnitTests/CopyAssembliesForTests.bat
+++
@@ -1,8 +0,0 @@
-﻿:: %1 :: For some reason this needs to be here or the first attempt to store this in a variable won't work.
-:: SET ProjectDir=%1
-:: SET BinDir="%ProjectDir%\Bin"
-:: 
-:: FOR /R %BinDir% %%F IN (*) DO (
-::   COPY /Y "%%F" %BinDir%
-:: )
-ECHO %1
