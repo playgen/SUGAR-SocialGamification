101334c740c1c2e969d117168979756ab3c56933
Jack Coldridge <jack@playgen.com>
2016-10-11 15:49:54 +0100

    update postcompile script

--- src/PlayGen.SUGAR.Client/CopyToUnity.bat
+++
@@ -1,6 +0,0 @@
-﻿%1 :: For some reason this needs to be here or the first attempt to store this in a variable won't work.
-%2
- SET BuildDir=%1
- SET ProjectDir=%2
-xcopy %BuildDir% "%ProjectDir%/../../../Client/Assets/Plugins/SUGAR" /Y /I
-
