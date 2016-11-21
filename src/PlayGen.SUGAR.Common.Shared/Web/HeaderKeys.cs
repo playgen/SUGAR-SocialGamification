2dc9421e4d5c9ad5bdfc647405a14281a858472c
Jared <jared@playgen.com>
2016-11-21 11:51:36 +0000

    httpcontext request header keys

---
+++ src/PlayGen.SUGAR.Common.Shared/Web/HeaderKeys.cs
@@ -0,0 +1,9 @@
+﻿namespace PlayGen.SUGAR.Common.Shared.Web
+{
+    public static class HeaderKeys
+    {
+        public const string Authorization = "Authorization";
+
+        public const string EvaluationNotifications = "EvaluationNotifications";
+    }
+}
