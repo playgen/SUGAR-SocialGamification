2373a9ea36d24725ab4e068fe2cc7d783967cf79
Unknown <solanylander@gmail.com>
2017-11-08 16:57:26 +0000

    Achievement resource rewards now add to existing

--- PlayGen.SUGAR.Server.Core/Controllers/RewardController.cs
+++ PlayGen.SUGAR.Server.Core/Controllers/RewardController.cs
@@ -3,6 +3,7 @@ using PlayGen.SUGAR.Server.Core.EvaluationEvents;
 using PlayGen.SUGAR.Server.EntityFramework;
 using PlayGen.SUGAR.Server.Model;
 using System;
+using System.Linq;
 
 namespace PlayGen.SUGAR.Server.Core.Controllers
 {
@@ -20,20 +21,39 @@ namespace PlayGen.SUGAR.Server.Core.Controllers
 		public bool AddReward(int? actorId, int? gameId, Reward reward)
 		{
 			var evaluationDataController = new EvaluationDataController(ContextFactory, reward.EvaluationDataCategory);
+			
+			var foundResources = evaluationDataController.Get(gameId, actorId, new[] { reward.EvaluationDataKey });
 
-			var evaluationData = new EvaluationData {
-				Key = reward.EvaluationDataKey,
-				GameId = gameId,    //TODO: handle the case where a global achievement has been completed for a specific game
-				ActorId = actorId,
-				Category = reward.EvaluationDataCategory,
-				EvaluationDataType = reward.EvaluationDataType,
-				Value = reward.Value
-			};
+			if (foundResources.Any())
+			{
+				EvaluationData resource = foundResources.Single();
 
-			evaluationDataController.Add(evaluationData);
+				var currentValue = long.Parse(resource.Value);
+				var addedValue = long.Parse(reward.Value);
+				resource.Value = (currentValue + addedValue).ToString();
 
-			Logger.Info($"Game Data: {evaluationData?.Id} for ActorId: {actorId}, GameId: {gameId}, Reward: {reward?.Id}");
+				evaluationDataController.Update(resource);
 
+				Logger.Info($"{resource?.Id} with Amount: {addedValue}");
+
+
+			}
+			else
+			{
+				var evaluationData = new EvaluationData {
+					Key = reward.EvaluationDataKey,
+					GameId = gameId,    //TODO: handle the case where a global achievement has been completed for a specific game
+					ActorId = actorId,
+					Category = reward.EvaluationDataCategory,
+					EvaluationDataType = reward.EvaluationDataType,
+					Value = reward.Value
+				};
+				
+				evaluationDataController.Add(evaluationData);
+
+				Logger.Info($"Game Data: {evaluationData?.Id} for ActorId: {actorId}, GameId: {gameId}, Reward: {reward?.Id}");
+
+			}
 			return true;
 		}
 	}
