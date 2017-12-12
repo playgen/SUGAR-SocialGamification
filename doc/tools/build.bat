4831605ea95f1ce5962a96b28146bd58330fb02c
Jared <jaredgglass@gmail.com>
2017-12-12 15:00:09 +0000

    doc build tool names made more specific

--- doc/tools/build.bat
+++ doc/tools/build.bat
@@ -1,5 +1,3 @@
 pushd ..\
 
-docfx metadata docfx.client.metadata.json
-docfx metadata docfx.server.metadata.json
 docfx build docfx.build.json
--- doc/tools/build_all_and_copy.bat
+++
@@ -1,7 +0,0 @@
-call build_swagger.bat
-
-call build.bat
-
-pushd tools\
-
-call copy_to_server.bat
