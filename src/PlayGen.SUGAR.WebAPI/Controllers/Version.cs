6b9522584cda4a2501b10bcc7f9b1e2a78abe89b
Jared <jaredgglass@gmail.com>
2018-03-21 16:59:46 +0000

    version controller and delay startup fixwed

---
+++ src/PlayGen.SUGAR.WebAPI/Controllers/Version.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Reflection;
+
+namespace PlayGen.SUGAR.WebAPI.Controllers
+{
+	[Route("api/[controller]")]
+	public class VersionController : Controller
+	{
+		// GET
+		[HttpGet]
+		public IActionResult Get()
+		{
+			return new OkObjectResult($"{DateTime.UtcNow}: {typeof(VersionController).GetTypeInfo().Assembly}. API Version: 0.0.0");
+		}
+	}
+}
--- src/PlayGen.SUGAR.WebAPI/delay-startup.sh
+++ src/PlayGen.SUGAR.WebAPI/delay-startup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [ -z ${SUGAR_MARIADB_PORT_3306_TCP} ]; then
-	echo "You must link this container with mariadb first"
+if [ -z ${SUGAR_V0_MARIADB_PORT_3306_TCP} ]; then
+	echo "You must link this container with sugar-v0-mariadb first"
 	exit 1
 fi
         
-until nc -w 1 -z sugar-mariadb 3306; do
-	echo "$(date) - waiting for sugar-mariadb..."
+until nc -w 1 -z sugar-v0-mariadb 3306; do
+	echo "$(date) - waiting for sugar-v0-mariadb..."
 	sleep 1
 done
 
