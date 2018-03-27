4de318328a0066ec96ddb31534e25a59d9514a26
Jared <jaredgglass@gmail.com>
2018-03-27 15:00:08 +0100

    added db port mapping to map port build files

--- docker/docker-compose_image_deploy_map-port.bat
+++
@@ -1,2 +0,0 @@
-docker-compose -f docker-compose.base.yml -f docker-compose.image.yml run --rm wait-for-db
-docker-compose -f docker-compose.base.yml -f docker-compose.image.yml -f docker-compose.map-port.yml up -d sugar-v1
--- docker/docker-compose_image_deploy_map-port.sh
+++
@@ -1,3 +0,0 @@
-#/bin/bash
-docker-compose -f docker-compose.base.yml -f docker-compose.image.yml run --rm wait-for-db
-docker-compose -f docker-compose.base.yml -f docker-compose.image.yml -f docker-compose.map-port.yml up -d sugar-v1
