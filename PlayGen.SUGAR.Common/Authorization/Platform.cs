0668b209b0e30d34858d3180b4c26050f81daef8
Ellis Spice <ellis@playgen.com>
2018-08-20 16:50:47 +0100

    Documentation added to Common classes

--- PlayGen.SUGAR.Common/Authorization/Platform.cs
+++ PlayGen.SUGAR.Common/Authorization/Platform.cs
@@ -2,7 +2,7 @@
 {
 	public class Platform
 	{
-		//Id of data (evaluationData, Achievements etc) no assigned to any actors or games
+		//Id of data (evaluationData, Achievements etc) not assigned to any actors or games
 		public const int GlobalId = 0;
 		//Id used to represent having ownership of all objects of a type
 		public const int AllId = -1;
