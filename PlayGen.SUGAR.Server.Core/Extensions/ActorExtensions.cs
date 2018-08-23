5f3810d62ff57f5641306284c785b6f24e84a7da
BAYMAX\Felix <felix@playgen.com>
2018-08-23 11:07:48 +0100

    update has cliam check to remove possible null reference

--- PlayGen.SUGAR.Server.Core/Extensions/ActorExtensions.cs
+++ PlayGen.SUGAR.Server.Core/Extensions/ActorExtensions.cs
@@ -11,31 +11,24 @@ namespace PlayGen.SUGAR.Server.Core.Extensions
     {
 		public static List<T> FilterPrivate<T>(this List<T> actors, ActorClaimController actorClaimController, int? requestingId) where T : Actor
 	    {
-			// TODO iterate over list
-			// admins have ClaimScope.Global, they can see all actors
 			foreach (var actor in actors)
 			{
-				var hasClaim = actorClaimController.GetActorClaimsForEntity(requestingId.Value, actor.Id, ClaimScope.User).Any() || actorClaimController.GetActorClaimsForEntity(requestingId.Value, actor.Id, ClaimScope.Group).Any();
+				var hasClaim = requestingId != null &&
+								(actorClaimController.GetActorClaimsForEntity(requestingId.Value, actor.Id, ClaimScope.User).Any() || actorClaimController.GetActorClaimsForEntity(requestingId.Value, actor.Id, ClaimScope.Group).Any());
 				if (!hasClaim && requestingId != actor.Id && actor.Private)
 				{
 					actors.Remove(actor);
 				}
 			}
-
-		 //   var claims = actorClaimController.GetActorClaimsForEntity(requestingId.Value, actor.Id, ClaimScope.User).Any() || actorClaimController.GetActorClaimsForEntity(requestingId.Value, actor.Id, ClaimScope.Group).Any(); ;
-
-			//if (requestingId == null || !actorClaimController.GetControlled(requestingId.Value).Any(c => c.ClaimScope == ClaimScope.Global))
-			//{
-			//    return actors.Where(a => !a.Private || a.Id == requestingId).ToList();
-		 //   }
 			 return actors;
 	    }
 
 	    public static T FilterPrivate<T>(this T actor,ActorClaimController actorClaimController, int? requestingId) where T : Actor
 	    {
-		    var hasClaim = actorClaimController.GetActorClaimsForEntity(requestingId.Value, actor.Id, ClaimScope.User).Any() || actorClaimController.GetActorClaimsForEntity(requestingId.Value, actor.Id, ClaimScope.Group).Any(); ;
+		    var hasClaim = requestingId  != null && 
+				(actorClaimController.GetActorClaimsForEntity(requestingId.Value, actor.Id, ClaimScope.User).Any() || actorClaimController.GetActorClaimsForEntity(requestingId.Value, actor.Id, ClaimScope.Group).Any()); 
 
-			if (requestingId == null || !hasClaim)
+			if (!hasClaim)
 			{
 				if (actor != null && actor.Private && actor.Id != requestingId)
 				{
