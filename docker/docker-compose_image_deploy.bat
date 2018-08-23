caa95f8a0379ee8c4d1974be9b3ae457fc87b992
Jared <jaredgglass@gmail.com>
2018-08-23 10:38:04 +0100

    SUGAR V2 - Global User Id set to NULL instead of 1 in prior commit which may break existing items that assume global is -1

--- docker/docker-compose_image_deploy.bat
+++ docker/docker-compose_image_deploy.bat
@@ -1,2 +1,2 @@
 docker-compose -f docker-compose.base.yml -f docker-compose.image.yml run --rm wait-for-db
-docker-compose -f docker-compose.base.yml -f docker-compose.image.yml up -d sugar-v1-demo
+docker-compose -f docker-compose.base.yml -f docker-compose.image.yml up -d sugar-v2
