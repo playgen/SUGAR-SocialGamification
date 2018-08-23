ef575f240030c6ba2d4a2cdcbba09eac774f090f
BAYMAX\Felix <felix@playgen.com>
2018-08-23 12:02:56 +0100

    update API Version

--- PlayGen.SUGAR.Common/APIVersion.cs
+++ PlayGen.SUGAR.Common/APIVersion.cs
@@ -21,7 +21,7 @@ namespace PlayGen.SUGAR.Common
 
 		public const int Minor = 3;
 
-		public const int Build = 2;
+		public const int Build = 3;
 
 		public static string Version => $"{Major}.{Minor}.{Build}";
 
